e25cd961c9cf3c121f91e90f47ec2d2696aca91c
vladimir-simek <minervladik@gmail.com>
2021-04-17 20:10:01 +0200

    memory game, random done

--- js/search.js
+++
@@ -1,5 +0,0 @@
-let q = document.getElementById("searchText").value;
-document.getElementById('searchButton').onclick = function() {
-    window.open('http://google.com/search?q='+q);
-    console.log("sadsa")
-};
