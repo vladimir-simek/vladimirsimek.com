f12084630515d08fa4d3b85e058d04879d1a4be1
vladimir-simek <minervladik@gmail.com>
2021-04-22 15:41:54 +0200

    MHM

--- js/memory.js
+++ js/memory.js
@@ -27,6 +27,25 @@ document.onload = function() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Get the modal
+    let modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+
     for (let i = 0; i < emojisList.length; i++) {
         let wtf = document.createTextNode("")
         let innerDiv = document.createElement("div")
